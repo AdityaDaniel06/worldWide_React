f52c23f2f0bd699f1b419733dc935d81c663ba2f
AdityaDaniel06 <danieladitya327@gmail.com>
2024-11-05 10:13:31 +0530

    v1 finished with useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
@@ -11,9 +12,11 @@ import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
-import { useEffect, useState } from "react";
+import City from "./components/City";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:8200";
+
 function App() {
   // global state for now
   const [cities, setCities] = useState([]);
@@ -44,19 +47,17 @@ function App() {
           <Route path="products" element={<Product />} />
           <Route path="/login" element={<Login />} />
           <Route path="/app" element={<AppLayout />}>
-            <Route
-              index
-              element={<CityList cities={cities} isLoading={isLoading} />}
-            />
+            <Route index element={<Navigate replace to="cities" />} />
             <Route
               path="cities"
               element={<CityList cities={cities} isLoading={isLoading} />}
             />
+            <Route path="cities/:id" element={<City />} />
             <Route
               path="countries"
               element={<CountryList cities={cities} isLoading={isLoading} />}
             />
-            <Route path="form" element={<p>form</p>} />
+            <Route path="form" element={<Form />} />
           </Route>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,13 +1,18 @@
+import { useSearchParams, useNavigate } from "react-router-dom";
 import styles from "./Map.module.css";
-import map_bg from "../../public/10-8.webp";
-import { Container } from "react-bootstrap";
+
 function Map() {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
   return (
-    <Container>
-      <div className={styles.mapContainer}>
-        <img src={map_bg} alt="map" className="w-100 " />
-      </div>
-    </Container>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <h1>Map</h1>
+      <h1>
+        Position {lat} , {lng}
+      </h1>
+    </div>
   );
 }
 
