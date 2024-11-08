727b30bff726793db30cd0636f48116ac2ae3932
AdityaDaniel06 <danieladitya327@gmail.com>
2024-11-08 16:55:53 +0530

    leftlet lib installed, working with maps

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # React + Vite
-This Project mainly focuses on routes in React.
-Project is built to track the travel destinations ,and see it on map.
 
+This Project mainly focuses on routes and Global State Management using Context Provider in React.
+Project is built to track the travel destinations ,and see it on map using Leaflet library.
 
 ![image](https://github.com/user-attachments/assets/2ff75b1a-bbf3-4195-96dd-64436639e2ce)
 
-![image](https://github.com/user-attachments/assets/5d49af29-b64f-49ed-8724-36eb63696328)
+![image](image.png)
+![image](image-1.png)
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,19 +1,75 @@
-import { useSearchParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import styles from "./Map.module.css";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvent,
+} from "react-leaflet";
+import { useSearchParams, useNavigate } from "react-router-dom";
+import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
-  const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const { cities } = useCities();
+  const [mapPosition, setMapPosition] = useState([23.259, 77.4]);
+  const [searchParams] = useSearchParams();
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
+
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Map</h1>
-      <h1>
-        Position {lat} , {lng}
-      </h1>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPosition}
+        zoom={10}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city?.emoji}</span>
+              <span>{city?.name}</span>
+            </Popup>
+          </Marker>
+        ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
+      </MapContainer>
     </div>
   );
 }
 
+// Custom Component to set the location icon on city we clicked
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+// component to route to forms on clicking on map anywhere
+function DetectClick() {
+  const navigate = useNavigate();
+  // const handleClick = (e) => {
+  //   navigate(`/city/add?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+  // };
+  // return <MapContainer onClick={handleClick} />;
+  useMapEvent({
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  });
+}
 export default Map;
